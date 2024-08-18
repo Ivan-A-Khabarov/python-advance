1b17b1502aa2740997efb8721f067ee571324cd8
Ivan-Khabarov <ivan@ihabarov.ru>
2024-08-18 21:19:39 +0300

    Added statements

---
+++ .venv/Lesson1/task1.py
@@ -0,0 +1,59 @@
+# import  math
+#
+# name = 'Alex'
+# MAX_COUNT = 1000
+# t = 10
+# a = 10
+# b = a == t
+#
+# age = None
+# age = 12
+# # print(id(age))
+#
+# pi = math.pi
+# e = math.e
+# # print(pi ** e)
+# # print(t == pi)
+# # print(b)
+#
+# a = 5
+# print(id(a))
+# a = 'Hello world'
+# print(id(a))
+# a = 3.14 / 3
+# print(id(a))
+#
+# print(name, age, 'text', sep=' (=^.^=) ', end='; ')
+# print('другой тексте')
+#
+# res = input('Print your text ')
+# print('Ты написал ' + res)
+#
+# age = int(input('Сколько тебе лет '))
+# print(age)
+#
+# ADULT = 18
+# age = int(input('Сколько тебе лет? '))
+# how_old = ADULT - age
+# print('Осталось до совершеннолетия', how_old , 'лет')
+
+
+# "==" - равно
+# "!=" - не равно
+# ">" - больше
+# ">=" - больше или равно
+# "<" - меньше
+# "<=" - меньше или равно
+
+# if,else,elif,match и case
+
+
+
+
+
+
+
+
+
+
+
