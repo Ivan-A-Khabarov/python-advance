28925a25d0751b7925c071b2b825d47f10119bfa
Ivan Khabarov <ivan@ihabarov.ru>
2024-09-04 10:21:16 +0300

    Добавлены методы сорт и реверс

---
+++ Lections/Lesson3/sorts_and_reverse/task_list_11.py
@@ -0,0 +1,7 @@
+# Функция sorted() позволяет сортировать список
+
+my_list = [4, 8, 2, 9, 1, 7, 2]
+sort_list = sorted(my_list)
+print(my_list, sort_list, sep='\n')
+rev_list = sorted(my_list, reverse=True)
+print(my_list, rev_list, sep='\n')
