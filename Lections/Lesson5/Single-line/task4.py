72f79b2516b94fed67024799a4b61ac82b1bf723
Ivan-Khabarov <ivan@ihabarov.ru>
2024-09-08 10:16:09 +0300

    Добавлена лекция 5

---
+++ Lections/Lesson5/Single-line/task4.py
@@ -0,0 +1,20 @@
+def gen(a: int, b: int) -> str:
+    if a > b:
+        a, b = b, a
+    for i in range(a, b + 1):
+        yield str(i)
+
+
+for item in gen(10, 1):
+    print(f'{item = }')
+
+# item = '1'
+# item = '2'
+# item = '3'
+# item = '4'
+# item = '5'
+# item = '6'
+# item = '7'
+# item = '8'
+# item = '9'
+# item = '10'
